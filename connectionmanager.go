794045d98f68dd21e4274867960cf0697ad89648
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2023-06-11 14:09:06 +0200

    Make use of `newConnectionManager` instead of creating the struct ourselves

--- connectionmanager.go
+++ connectionmanager.go
@@ -23,14 +23,18 @@ type connectionManager struct {
 	quitChannel chan struct{}
 }
 
-func newConnectionManager(host string, messageBus messageBusType, quitChannel chan struct{}) *connectionManager {
+func newConnectionManager(host string, connectionLimit int, topicLimit int, messageBus messageBusType, quitChannel chan struct{}) *connectionManager {
 	return &connectionManager{
 		host: host,
 
-		connectionLimit: 10,
-		topicLimit:      49,
-		messageBus:      messageBus,
-		quitChannel:     quitChannel,
+		connectionLimit:      connectionLimit,
+		connectionLimitMutex: &sync.RWMutex{},
+
+		topicLimit:      topicLimit,
+		topicLimitMutex: &sync.RWMutex{},
+
+		messageBus:  messageBus,
+		quitChannel: quitChannel,
 	}
 }
 
