af1f849d0547cf25c3979594f190eff0cabf78aa
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2023-06-17 17:58:51 +0200

    generalize variable naming in moderation/subscribe tests

--- subscribe_test.go
+++ subscribe_test.go
@@ -30,7 +30,7 @@ func TestParseSubscribeEvent(t *testing.T) {
 	type testCase struct {
 		label            string
 		input            string
-		isSubscribeMsg   bool
+		isValidMsg       bool
 		expected         *SubscribeEvent
 		expectedErr      error
 		expectedOuterErr error
@@ -38,9 +38,9 @@ func TestParseSubscribeEvent(t *testing.T) {
 
 	testCases := []testCase{
 		{
-			label:          "Prime resubscription with no emotes & no message",
-			input:          `{"type":"MESSAGE","data":{"topic":"channel-subscribe-events-v1.11148817","message":"{\"benefit_end_month\":0,\"user_name\":\"randers\",\"display_name\":\"randers\",\"channel_name\":\"pajlada\",\"user_id\":\"40286300\",\"channel_id\":\"11148817\",\"time\":\"2023-06-11T10:44:06.975336457Z\",\"sub_message\":{\"message\":\"\",\"emotes\":null},\"sub_plan\":\"Prime\",\"sub_plan_name\":\"look at those shitty emotes, rip $5 LUL\",\"months\":0,\"cumulative_months\":54,\"context\":\"resub\",\"is_gift\":false,\"multi_month_duration\":0}"}}`,
-			isSubscribeMsg: true,
+			label:      "Prime resubscription with no emotes & no message",
+			input:      `{"type":"MESSAGE","data":{"topic":"channel-subscribe-events-v1.11148817","message":"{\"benefit_end_month\":0,\"user_name\":\"randers\",\"display_name\":\"randers\",\"channel_name\":\"pajlada\",\"user_id\":\"40286300\",\"channel_id\":\"11148817\",\"time\":\"2023-06-11T10:44:06.975336457Z\",\"sub_message\":{\"message\":\"\",\"emotes\":null},\"sub_plan\":\"Prime\",\"sub_plan_name\":\"look at those shitty emotes, rip $5 LUL\",\"months\":0,\"cumulative_months\":54,\"context\":\"resub\",\"is_gift\":false,\"multi_month_duration\":0}"}}`,
+			isValidMsg: true,
 			expected: &SubscribeEvent{
 				ChannelID:            "11148817",
 				ChannelName:          "pajlada",
@@ -66,9 +66,9 @@ func TestParseSubscribeEvent(t *testing.T) {
 			expectedOuterErr: nil,
 		},
 		{
-			label:          "Prime resubscription with no emotes",
-			input:          `{"type":"MESSAGE","data":{"topic":"channel-subscribe-events-v1.11148817","message":"{\"benefit_end_month\":0,\"user_name\":\"supersaintnick\",\"display_name\":\"SuperSaintNick\",\"channel_name\":\"pajlada\",\"user_id\":\"123747906\",\"channel_id\":\"11148817\",\"time\":\"2023-06-11T11:39:00.678953302Z\",\"sub_message\":{\"message\":\"pajaCheese\",\"emotes\":null},\"sub_plan\":\"Prime\",\"sub_plan_name\":\"look at those shitty emotes, rip $5 LUL\",\"months\":0,\"cumulative_months\":2,\"context\":\"resub\",\"is_gift\":false,\"multi_month_duration\":0}"}}`,
-			isSubscribeMsg: true,
+			label:      "Prime resubscription with no emotes",
+			input:      `{"type":"MESSAGE","data":{"topic":"channel-subscribe-events-v1.11148817","message":"{\"benefit_end_month\":0,\"user_name\":\"supersaintnick\",\"display_name\":\"SuperSaintNick\",\"channel_name\":\"pajlada\",\"user_id\":\"123747906\",\"channel_id\":\"11148817\",\"time\":\"2023-06-11T11:39:00.678953302Z\",\"sub_message\":{\"message\":\"pajaCheese\",\"emotes\":null},\"sub_plan\":\"Prime\",\"sub_plan_name\":\"look at those shitty emotes, rip $5 LUL\",\"months\":0,\"cumulative_months\":2,\"context\":\"resub\",\"is_gift\":false,\"multi_month_duration\":0}"}}`,
+			isValidMsg: true,
 			expected: &SubscribeEvent{
 				ChannelID:            "11148817",
 				ChannelName:          "pajlada",
@@ -94,9 +94,9 @@ func TestParseSubscribeEvent(t *testing.T) {
 			expectedOuterErr: nil,
 		},
 		{
-			label:          "Non-prime resubscription with emotes",
-			input:          `{"type":"MESSAGE","data":{"topic":"channel-subscribe-events-v1.11148817","message":"{\"benefit_end_month\":0,\"user_name\":\"nerixyz\",\"display_name\":\"nerixyz\",\"channel_name\":\"pajlada\",\"user_id\":\"129546453\",\"channel_id\":\"11148817\",\"time\":\"2023-06-11T11:22:45.552399915Z\",\"sub_message\":{\"message\":\"forsen pajaW FeelsDankMan m0xyGift\",\"emotes\":[{\"start\":7,\"end\":11,\"id\":\"80481\"},{\"start\":26,\"end\":33,\"id\":\"303660952\"}]},\"sub_plan\":\"1000\",\"sub_plan_name\":\"look at those shitty emotes, rip $5 LUL\",\"months\":0,\"cumulative_months\":12,\"context\":\"resub\",\"is_gift\":false,\"multi_month_duration\":0}"}}`,
-			isSubscribeMsg: true,
+			label:      "Non-prime resubscription with emotes",
+			input:      `{"type":"MESSAGE","data":{"topic":"channel-subscribe-events-v1.11148817","message":"{\"benefit_end_month\":0,\"user_name\":\"nerixyz\",\"display_name\":\"nerixyz\",\"channel_name\":\"pajlada\",\"user_id\":\"129546453\",\"channel_id\":\"11148817\",\"time\":\"2023-06-11T11:22:45.552399915Z\",\"sub_message\":{\"message\":\"forsen pajaW FeelsDankMan m0xyGift\",\"emotes\":[{\"start\":7,\"end\":11,\"id\":\"80481\"},{\"start\":26,\"end\":33,\"id\":\"303660952\"}]},\"sub_plan\":\"1000\",\"sub_plan_name\":\"look at those shitty emotes, rip $5 LUL\",\"months\":0,\"cumulative_months\":12,\"context\":\"resub\",\"is_gift\":false,\"multi_month_duration\":0}"}}`,
+			isValidMsg: true,
 			expected: &SubscribeEvent{
 				ChannelID:            "11148817",
 				ChannelName:          "pajlada",
@@ -133,9 +133,9 @@ func TestParseSubscribeEvent(t *testing.T) {
 			expectedOuterErr: nil,
 		},
 		{
-			label:          "Non-prime resubscription with streak",
-			input:          `{"type":"MESSAGE","data":{"topic":"channel-subscribe-events-v1.11148817","message":"{\"benefit_end_month\":0,\"user_name\":\"rotten_pizza\",\"display_name\":\"rotten_pizza\",\"channel_name\":\"pajlada\",\"user_id\":\"69478316\",\"channel_id\":\"11148817\",\"time\":\"2023-06-11T11:33:01.381805678Z\",\"sub_message\":{\"message\":\"!#playsound cheese pajaCheese\",\"emotes\":null},\"sub_plan\":\"1000\",\"sub_plan_name\":\"look at those shitty emotes, rip $5 LUL\",\"months\":0,\"cumulative_months\":24,\"streak_months\":24,\"context\":\"resub\",\"is_gift\":false,\"multi_month_duration\":0}"}}`,
-			isSubscribeMsg: true,
+			label:      "Non-prime resubscription with streak",
+			input:      `{"type":"MESSAGE","data":{"topic":"channel-subscribe-events-v1.11148817","message":"{\"benefit_end_month\":0,\"user_name\":\"rotten_pizza\",\"display_name\":\"rotten_pizza\",\"channel_name\":\"pajlada\",\"user_id\":\"69478316\",\"channel_id\":\"11148817\",\"time\":\"2023-06-11T11:33:01.381805678Z\",\"sub_message\":{\"message\":\"!#playsound cheese pajaCheese\",\"emotes\":null},\"sub_plan\":\"1000\",\"sub_plan_name\":\"look at those shitty emotes, rip $5 LUL\",\"months\":0,\"cumulative_months\":24,\"streak_months\":24,\"context\":\"resub\",\"is_gift\":false,\"multi_month_duration\":0}"}}`,
+			isValidMsg: true,
 			expected: &SubscribeEvent{
 				ChannelID:            "11148817",
 				ChannelName:          "pajlada",
@@ -161,9 +161,9 @@ func TestParseSubscribeEvent(t *testing.T) {
 			expectedOuterErr: nil,
 		},
 		{
-			label:          "Gift subscription",
-			input:          `{"type":"MESSAGE","data":{"topic":"channel-subscribe-events-v1.11148817","message":"{\"benefit_end_month\":0,\"user_name\":\"pajlada\",\"display_name\":\"pajlada\",\"channel_name\":\"pajlada\",\"user_id\":\"11148817\",\"channel_id\":\"11148817\",\"recipient_id\":\"29294552\",\"recipient_user_name\":\"ultrainstinctcocabear\",\"recipient_display_name\":\"ultrainstinctcocabear\",\"time\":\"2023-06-11T11:36:21.198150864Z\",\"sub_message\":{\"message\":\"\",\"emotes\":null},\"sub_plan\":\"1000\",\"sub_plan_name\":\"look at those shitty emotes, rip $5 LUL\",\"months\":2,\"context\":\"subgift\",\"is_gift\":true,\"multi_month_duration\":1}"}}`,
-			isSubscribeMsg: true,
+			label:      "Gift subscription",
+			input:      `{"type":"MESSAGE","data":{"topic":"channel-subscribe-events-v1.11148817","message":"{\"benefit_end_month\":0,\"user_name\":\"pajlada\",\"display_name\":\"pajlada\",\"channel_name\":\"pajlada\",\"user_id\":\"11148817\",\"channel_id\":\"11148817\",\"recipient_id\":\"29294552\",\"recipient_user_name\":\"ultrainstinctcocabear\",\"recipient_display_name\":\"ultrainstinctcocabear\",\"time\":\"2023-06-11T11:36:21.198150864Z\",\"sub_message\":{\"message\":\"\",\"emotes\":null},\"sub_plan\":\"1000\",\"sub_plan_name\":\"look at those shitty emotes, rip $5 LUL\",\"months\":2,\"context\":\"subgift\",\"is_gift\":true,\"multi_month_duration\":1}"}}`,
+			isValidMsg: true,
 			expected: &SubscribeEvent{
 				ChannelID:            "11148817",
 				ChannelName:          "pajlada",
@@ -189,9 +189,9 @@ func TestParseSubscribeEvent(t *testing.T) {
 			expectedOuterErr: nil,
 		},
 		{
-			label:          "Anonymous gift subscription",
-			input:          `{"type":"MESSAGE","data":{"topic":"channel-subscribe-events-v1.11148817","message":"{\"benefit_end_month\":0,\"channel_name\":\"pajlada\",\"channel_id\":\"11148817\",\"recipient_id\":\"72902587\",\"recipient_user_name\":\"slch000\",\"recipient_display_name\":\"SLCH000\",\"time\":\"2023-06-11T11:43:08.367893236Z\",\"sub_message\":{\"message\":\"\",\"emotes\":null},\"sub_plan\":\"1000\",\"sub_plan_name\":\"look at those shitty emotes, rip $5 LUL\",\"months\":3,\"context\":\"anonsubgift\",\"is_gift\":true,\"multi_month_duration\":1}"}}`,
-			isSubscribeMsg: true,
+			label:      "Anonymous gift subscription",
+			input:      `{"type":"MESSAGE","data":{"topic":"channel-subscribe-events-v1.11148817","message":"{\"benefit_end_month\":0,\"channel_name\":\"pajlada\",\"channel_id\":\"11148817\",\"recipient_id\":\"72902587\",\"recipient_user_name\":\"slch000\",\"recipient_display_name\":\"SLCH000\",\"time\":\"2023-06-11T11:43:08.367893236Z\",\"sub_message\":{\"message\":\"\",\"emotes\":null},\"sub_plan\":\"1000\",\"sub_plan_name\":\"look at those shitty emotes, rip $5 LUL\",\"months\":3,\"context\":\"anonsubgift\",\"is_gift\":true,\"multi_month_duration\":1}"}}`,
+			isValidMsg: true,
 			expected: &SubscribeEvent{
 				ChannelID:            "11148817",
 				ChannelName:          "pajlada",
@@ -222,9 +222,9 @@ func TestParseSubscribeEvent(t *testing.T) {
 		c.Run(testCase.label, func(c *qt.C) {
 			outerMessage, err := parseOuterMessage([]byte(testCase.input))
 			c.Assert(err, qt.Equals, testCase.expectedOuterErr)
-			c.Assert(isSubscribeEventTopic(outerMessage.Data.Topic), qt.Equals, testCase.isSubscribeMsg)
+			c.Assert(isSubscribeEventTopic(outerMessage.Data.Topic), qt.Equals, testCase.isValidMsg)
 
-			if testCase.isSubscribeMsg {
+			if testCase.isValidMsg {
 				// Only test parsing if we expect the input message to be an actual subscribe message
 				innerMessageBytes := []byte(outerMessage.Data.Message)
 				actual, err := parseSubscribeEvent([]byte(innerMessageBytes))
